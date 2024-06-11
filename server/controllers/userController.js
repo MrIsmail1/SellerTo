0624627934f711824f5c1223c9fc36f8929ec250
Sébastien <sebramirez93100@gmail.com>
2024-06-11 20:41:55 +0200

    [SLT-34] : Suppression compte et anonymisation des données (back)

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -13,3 +13,18 @@ export const getUserProfile = async (req, res) => {
     res.status(500);
   }
 };
+
+export const deleteUserAccount = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.user.id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    await user.anonymize();
+    res.clearCookie('JWT');
+    res.status(200).json({ message: 'User account deleted' });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getUserProfile } from '../controllers/userController.js';
+import { getUserProfile, deleteUserAccount } from '../controllers/userController.js';
 import { checkAuth } from '../middlewares/checkAuth.js';
 
 const router = Router();
 
 router.get('/', checkAuth, getUserProfile);
+router.delete('/delete', checkAuth, deleteUserAccount);
 
 export default router;
