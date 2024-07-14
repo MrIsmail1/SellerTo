cdee9c08792ae6913bd9efa93679ccc24c92eee8
Sébastien <sebramirez93100@gmail.com>
2024-07-14 14:38:37 +0200

    [SLT-28] : Gestion des stocks

---
+++ server/models/postgres/stockModel.js
@@ -0,0 +1,62 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../../config/database.js';
+import Products from './productModel.js';  // Assurez-vous que le chemin est correct
+
+const Stock = sequelize.define('Stock', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    productId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Products',
+            key: 'id',
+        },
+    },
+    quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    event: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    operationType: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isIn: [['ADD', 'REMOVE']]
+        }
+    },
+}, {
+    timestamps: true,
+    hooks: {
+        afterCreate: async (stock, options) => {
+            await updateProductStock(stock.productId);
+        },
+        afterUpdate: async (stock, options) => {
+            await updateProductStock(stock.productId);
+        },
+        afterDestroy: async (stock, options) => {
+            await updateProductStock(stock.productId);
+        }
+    }
+});
+
+const updateProductStock = async (productId) => {
+    // Calculez le stock total pour le produit donné
+    const stocks = await Stock.findAll({ where: { productId } });
+    const totalStock = stocks.reduce((total, stock) => {
+        return stock.operationType === 'ADD' ? total + stock.quantity : total - stock.quantity;
+    }, 0);
+
+    // Mettez à jour la colonne product_stock dans la table Products
+    await Products.update({ product_stock: totalStock }, {
+        where: { id: productId }
+    });
+};
+
+export default Stock;
