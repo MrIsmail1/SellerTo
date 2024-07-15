584b5c20e5af84ee3859739a56fccd7eaa421337
Hamza Mahmood <hamza.mahmood@outlook.fr>
2024-07-15 19:07:16 +0200

    clean code and add refund

--- server/sequelizerc.js
+++ server/sequelizerc.js
@@ -1,8 +1,8 @@
 import path from 'path';
 
 module.exports = {
-  'config': path.resolve('src', 'config', 'config.cjs'),
-  'models-path': path.resolve('src', 'models'),
-  'migrations-path': path.resolve('src', 'migrations'),
-  'seeders-path': path.resolve('src', 'seeders')
+  'config': path.resolve('server', 'config', 'config.cjs'),
+  'models-path': path.resolve('server', 'models'),
+  'migrations-path': path.resolve('server', 'migrations'),
+  'seeders-path': path.resolve('server', 'seeders')
 };
