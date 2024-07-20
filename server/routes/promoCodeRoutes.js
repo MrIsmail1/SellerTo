758c810b89a6fc37f319f584ee786ae958ab605d
Sébastien <sebramirez93100@gmail.com>
2024-07-20 19:18:16 +0200

    SLT-79 : Ajoute routes code promo

--- server/routes/promoCodeRoutes.js
+++ server/routes/promoCodeRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { validatePromoCode, createPromoCode, updatePromoCode, deletePromoCode, getAllPromoCodes } from '../controllers/promoCodeController.js';
+import {checkRole} from "../middlewares/checkRole.js";
 
 const router = express.Router();
 
-router.get('/', getAllPromoCodes);
-router.post('/', createPromoCode);
-router.put('/:id', updatePromoCode);
+router.get('/', getAllPromoCodes, checkRole(["Admin", "SuperAdmin"]));
+router.post('/', createPromoCode, checkRole(["Admin", "SuperAdmin"]));
+router.put('/:id', updatePromoCode, checkRole(["Admin", "SuperAdmin"]));
 router.post('/validate', validatePromoCode);
-router.delete('/:id', deletePromoCode);
+router.delete('/:id', deletePromoCode, checkRole(["Admin", "SuperAdmin"]));
 
 export default router;
