042edfdaab934a19d28a51121380e4e3595fbf5b
Sébastien <sebramirez93100@gmail.com>
2024-07-20 13:57:32 +0200

    SLT-79 : Ajoute routes code promo

--- server/routes/promoCodeRoutes.js
+++ server/routes/promoCodeRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { validatePromoCode } from '../controllers/promoCodeController.js';
+import { validatePromoCode, createPromoCode, updatePromoCode, deletePromoCode, getAllPromoCodes } from '../controllers/promoCodeController.js';
 
 const router = express.Router();
 
+router.get('/', getAllPromoCodes);
+router.post('/', createPromoCode);
+router.put('/:id', updatePromoCode);
 router.post('/validate', validatePromoCode);
+router.delete('/:id', deletePromoCode);
 
 export default router;
